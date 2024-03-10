5d075dcd995e535d8aff3632f3c76a28d12efd68
leonschreuder <leon.schreuder@protonmail.com>
2024-03-10 21:15:34 +0100

    doc: update main gendocs + regenerate docs using new functionality

--- docsrc/gendocs.lua
+++ docsrc/gendocs.lua
@@ -1,11 +1,9 @@
 
 require('sourcecode_parser')
 require('doctree_printer')
+require('template_printer')
 
 local filePath = "../plugin/searchalot.vim"
 local docTree = ParseFile(filePath)
-local lines = DocTreeToLines(docTree)
-
-for _, line in pairs(lines) do
-  print(line)
-end
+local mappedLines = DocTreeToLines(docTree)
+ApplyTemplate('./searchalot_template.txt', "../doc/searchalot.txt", mappedLines)
