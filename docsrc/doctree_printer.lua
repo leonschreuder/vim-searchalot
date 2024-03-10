4a61c090a7dbc67c6fdf022dbd0ae76842351ab6
leonschreuder <leon.schreuder@protonmail.com>
2024-03-10 21:14:44 +0100

    doc: modify line printing logic to return items grouped by type

--- docsrc/doctree_printer.lua
+++ docsrc/doctree_printer.lua
@@ -1,7 +1,24 @@
 
+local function insertAll(tbl, items)
+  for _, value in ipairs( items ) do
+    tbl[#tbl+1] = value
+  end
+end
+
+local function insertAllForKey(table, key, valuesToAdd)
+    local newValues = {}
+    if table[key] ~= nil then
+      newValues = table[key]
+    end
+    insertAll(newValues, valuesToAdd)
+    table[key] = newValues
+end
+
 function DocTreeToLines(docTree)
   local textIndent = string.rep(" ", 28) -- 29 for the indent - 1 for the concat
   local linkIndent = string.rep(" ", 44)
+
+  local mappedLines = {}
 
   local lines = {}
   for _, block in pairs(docTree) do
@@ -9,12 +26,12 @@ function DocTreeToLines(docTree)
     -- print links
     for _, command in pairs(block.identifiers) do
       local pureComandName = string.gsub(string.sub(command, 1), "%s.*$", "")
-      table.insert(lines, linkIndent .. "searchalot-" .. pureComandName)
+      lines[#lines+1] = linkIndent .. "searchalot-" .. pureComandName
     end
 
     -- print identifiers with arguments
     for _, command in pairs(block.identifiers) do
-      table.insert(lines, command)
+      lines[#lines+1] = command
     end
 
     -- Print command description.
@@ -41,15 +58,19 @@ function DocTreeToLines(docTree)
         if #newline <= 78 then
           line = newline
         else
-          table.insert(lines, line)
+          lines[#lines+1] = line
           line = textIndent .. " " .. token
         end
       end
     end
 
-    table.insert(lines, line)
-    table.insert(lines, "") -- empty line to separate blocks
+    lines[#lines+1] = line
+    lines[#lines+1] = "" -- empty line to separate blocks
+
+    insertAllForKey(mappedLines, block.type, lines)
+    lines = {}
+
   end
-  return lines
+  return mappedLines
 end
 
--- docsrc/doctree_printer_test.lua
+++ docsrc/doctree_printer_test.lua
@@ -6,25 +6,27 @@ function Test_should_convert_doctree_to_lines()
   local doctree = {
     {
       identifiers = { ":Command {args}", ":Cmd {args}"},
-      text =  { "Run a search through all files in the current working directory." }
+      text =  { "Run a search through all files in the current working directory." },
+      type = "COMMAND",
     },
     {
       identifiers = { ":Command2 {args}"},
-      text =  { "Same as |:Sal| but open the result in the location list." }
+      text =  { "Same as |:Sal| but open the result in the location list." },
+      type = "COMMAND",
     },
   }
 
   local result = DocTreeToLines(doctree)
 
-
-  luaunit.assertEquals(result[1], "                                            searchalot-:Command")
-  luaunit.assertEquals(result[2], "                                            searchalot-:Cmd")
-  luaunit.assertEquals(result[3], ":Command {args}")
-  luaunit.assertEquals(result[4], ":Cmd {args}                  Run a search through all files in the current")
-  luaunit.assertEquals(result[5], "                             working directory.")
-  luaunit.assertEquals(result[6], "")
-  luaunit.assertEquals(result[7], "                                            searchalot-:Command2")
-  luaunit.assertEquals(result[8], ":Command2 {args}             Same as |:Sal| but open the result in the")
-  luaunit.assertEquals(result[9], "                             location list.")
+  local commandLines = result["COMMAND"]
+  luaunit.assertEquals(commandLines[1], "                                            searchalot-:Command")
+  luaunit.assertEquals(commandLines[2], "                                            searchalot-:Cmd")
+  luaunit.assertEquals(commandLines[3], ":Command {args}")
+  luaunit.assertEquals(commandLines[4], ":Cmd {args}                  Run a search through all files in the current")
+  luaunit.assertEquals(commandLines[5], "                             working directory.")
+  luaunit.assertEquals(commandLines[6], "")
+  luaunit.assertEquals(commandLines[7], "                                            searchalot-:Command2")
+  luaunit.assertEquals(commandLines[8], ":Command2 {args}             Same as |:Sal| but open the result in the")
+  luaunit.assertEquals(commandLines[9], "                             location list.")
 end
 
